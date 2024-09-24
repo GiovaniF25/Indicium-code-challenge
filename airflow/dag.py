36d5b7431f621fc0b0b9d4bdd4037007f820706b
Giovani M. Ferrari <106926901+GiovaniF25@users.noreply.github.com>
2024-09-24 14:45:20 -0300

    Update dag.py

--- airflow/dag.py
+++ airflow/dag.py
@@ -1,9 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from pathlib import Path
 
-# Defining default DAG parameters
 default_args = {
     'owner': 'GIOVANI M FERRARI',
     'depends_on_past': False,
@@ -12,24 +10,20 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-# Creating the DAG object
 dag = DAG(
     'indicium_data_pipeline',
     default_args=default_args,
-    schedule_interval=timedelta(days=1),  # Run daily
+    schedule_interval=timedelta(days=1),
 )
 
-# Function encapsulating the data extraction logic
 def extract_data(**kwargs):
     from extract_data import extract_data_task
     extract_data_task()
 
-# Function encapsulating the data loading logic
 def load_data(**kwargs):
     from load_data import load_data_task
     load_data_task()
 
-# Creating PythonOperator tasks to execute the extraction and loading functions
 extract_task = PythonOperator(
     task_id='extract_data',
     python_callable=extract_data,
@@ -44,5 +38,4 @@ load_task = PythonOperator(
     dag=dag,
 )
 
-# Defining the execution order of tasks
 extract_task >> load_task
